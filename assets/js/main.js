4d7080929b13589b8b2151d4f2a275eec0dd4767
Alexandra Dunham <alexandra.carternz@gmail.com>
2017-10-06 12:47:07 +1300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+     var questions = $('.v3-input-wrap');
+      var dots = $('.form-dots a');
+
+      $(questions).hide();
+      // hides next arrow for last item
+      $(questions).last().find('.next-click').hide();
+      //shows first question on load
+      $(questions).first().addClass("active").show();
+      //makes first dot active on load
+      $(dots).first().addClass("active");
+
+      //hides questions with transition
+      var hideQuestions = function() {
+        $(questions).addClass("up")
+        setTimeout(function(){
+          $(questions).hide();
+          $(questions).removeClass("up")
+          $(questions).removeClass("active")
+        },200)
+      }
+
+      //when each dot is clicked, hide questions and show the one associated with dot
+      $(dots).each(function(key, val){
+        $(val).click(function(){
+
+          hideQuestions();
+          //changes which dot has active class
+          $(dots).removeClass('active');
+          $(val).addClass('active');
+
+          //shows the correct item according to data-show
+          var id = $(val).attr('data-show');
+          var showMe = id  + '-wrap';
+          //transition stuff
+          setTimeout(function(){
+            $(showMe).show();
+            $(showMe).addClass("active");
+          },200)
+
+        });
+      });
+      $('.next-click').click(function(){
+        hideQuestions();
+        //shows the correct item according to data-show
+        var id  = $(this).attr('data-show');
+        var showMe = id  + '-wrap';
+        //changes which dot has active class
+        $(dots).each(function(key, val){
+          if($(val).attr('data-show') == id) {
+            $(val).addClass('active');
+          }
+          else {
+            $(val).removeClass('active');
+          }
+        });
+        //transition stuff
+        setTimeout(function(){
+          $(showMe).show();
+          $(showMe).addClass("active");
+        },200)
+      });
+});
